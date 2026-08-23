26c99a017c860832d20ef9a9f3508feb8faaa6d2
Amaraa124 <saruulamaraa99@gmail.com>
2026-08-23 20:54:41 +0800

    user

--- src/main/java/com/example/models/User.java
+++ src/main/java/com/example/models/User.java
@@ -1,5 +1,76 @@
 package com.example.models;
 
 public class User {
+    private int id;
+    private String username;
+    private int totalScore;
+    private int lives;
+    private int coins;
+
+    public User(int id, String username, int totalScore, int lives, int coins) {
+        this.id = id;
+        this.username = username;
+        this.totalScore = totalScore;
+        this.lives = lives;
+        this.coins = coins;
+    }
+
+    public User(String username, int totalScore, int lives, int coins) {
+        this.username = username;
+        this.totalScore = totalScore;
+        this.lives = lives;
+        this.coins = coins;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(int totalScore) {
+        this.totalScore = totalScore;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
+
+    public int getCoins() {
+        return coins;
+    }
+
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", totalScore=" + totalScore +
+                ", lives=" + lives +
+                ", coins=" + coins +
+                '}';
+    }
 
 }
