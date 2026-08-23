deab4c9a1231060e760acd9ff6043879bae8ea68
Amaraa124 <saruulamaraa99@gmail.com>
2026-08-23 21:12:27 +0800

    app.java zasvar

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,23 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.example.controllers.SpinWheelController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        SpinWheelController mainUI = new SpinWheelController();
+        Scene scene = new Scene(mainUI, 500, 450);
+
+        stage.setTitle("Trivia Battle - Lucky Spin");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
